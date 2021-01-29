fd7dd993516c6ac02040442f9de22476612f8120
fangwu.@chinapex.com <fang.wu@chinapex.com>
2021-01-29 15:48:01 +0800

    truffle test begin

--- test/phoenix_test.js
+++ test/phoenix_test.js
@@ -1,13 +1,22 @@
 const phoenixClient = artifacts.require("phoenixClient");
-// var Web3 = require('web3');
-// var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"))
+const web3 = require("web3")
 contract('phoenixClient', () => {
     let oracle;
     beforeEach(async () => {
         oracle = await phoenixClient.deployed();
     });
     it("returns the value it was initialized with", async () => {
-        let value = await oracle.value1.call();
-        assert.equal(1, value);
+        let value1 = await oracle.value1.call();
+        //let value = await oracle.value.call();
+        assert.equal(1, value1);
+        var value = web3.utils.toHex("hellp apex");
+        console.log(value); // "1000000000000000000"
+        //assert.equal(web3.utils.toHex("hellp apex"), web3.utils)
+    });
+
+    it("sets the value", async () => {
+        await oracle.setValue1(2);
+        let value1 = await oracle.value1.call();
+        assert.equal(2, value1);
     });
 });
